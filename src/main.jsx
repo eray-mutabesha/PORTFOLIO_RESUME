282d105222a418d6b9208f02b30bf5676909fb57
eray-mutabesha <107131060+eray-mutabesha@users.noreply.github.com>
2024-07-24 11:55:04 -0700

    slim_cong

--- src/main.jsx
+++ src/main.jsx
@@ -27,9 +27,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     
      <ThemeProvider theme={theme}>
-     <App />
-     {/* </ThemeProvider> */}
-    <ParticleBackground />
+      <App />
+      </ThemeProvider> 
+    
     
   </React.StrictMode>,
 )
